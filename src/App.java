bd6dec58b38b68f736b794e42b4420f60f9573f9
mienappels <elivholland@gmail.com>
2022-09-17 19:10:36 +0200

    Changed README

--- src/App.java
+++ src/App.java
@@ -5,7 +5,6 @@ class Main{
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Rock, Paper, Sciccors");
-
         String pick = myObj.nextLine();
         pick.trim();
         Long PCpickL = (Math.round(Math.random() * 100));
